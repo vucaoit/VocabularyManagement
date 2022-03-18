e4fb3df4ade4d9ed2287791f38752f5c1a963ebb
vucaoit <86138777+vucaoit@users.noreply.github.com>
2022-03-18 15:30:44 +0700

    modifed:register

--- app/src/main/java/com/example/vocabularymanagement/views/register/RegisterFragment.kt
+++ app/src/main/java/com/example/vocabularymanagement/views/register/RegisterFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.vocabularymanagement.R
 
 class RegisterFragment : Fragment() {
-
-
+    private lateinit var btnCreate: Button
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,5 +25,17 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnCreate = view.findViewById(R.id.register_btn_create)
+        btnLogin = view.findViewById(R.id.register_btn_login)
+        btnLogin.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_registerFragment2_to_loginFragment2)
+        }
+        btnCreate.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_registerFragment2_to_loginFragment2)
+        }
+    }
 }
